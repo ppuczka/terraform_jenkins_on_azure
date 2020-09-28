4ed4417b59f96836ae18223deab5d6a8e5fe2f2e
Przemyslaw Puczka <p.puczka@gmail.com>
2020-09-28 21:49:22 +0200

    output ips

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,7 @@
-# output "tls_private_key" { value = tls_private_key.azure_pk.private_key_pem }
+output "docker_host_public_ip" { 
+    value = azure_rm_public_ip.ip-2.ip_address
+}
+
+output "jenkins_host_public_ip" {
+    value = azure_rm_public_ip.ip-1.ip_address
+}
