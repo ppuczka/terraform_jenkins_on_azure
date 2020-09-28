e361bd77ee9e19766efdcd1f1c0499a13562f301
Przemyslaw Puczka <p.puczka@gmail.com>
2020-09-28 21:50:26 +0200

    typho

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "docker_host_public_ip" { 
-    value = azure_rm_public_ip.ip-2.ip_address
+    value = azurerm_public_ip.ip-2.ip_address
 }
 
 output "jenkins_host_public_ip" {
-    value = azure_rm_public_ip.ip-1.ip_address
+    value = azurerm_public_ip.ip-1.ip_address
 }
